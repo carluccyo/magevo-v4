2885ee5ff9ffe6a3e5dcec71c74e6a4c02d5df33
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-04-30 18:49:53 +0200

    admin module

--- src/app/can-deactivate-guard.service.ts
+++ src/app/can-deactivate-guard.service.ts
@@ -14,8 +14,3 @@ export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate>
 }
 
 
-/*
-Copyright 2017 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
