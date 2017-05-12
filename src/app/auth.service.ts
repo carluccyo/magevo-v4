7f7e9bcfa4eb79de1a6439947582a6995397a572
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-05-12 14:43:54 +0200

    add jquery and bootstrap

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -20,10 +20,3 @@ export class AuthService {
     this.isLoggedIn = false;
   }
 }
-
-
-/*
-Copyright 2017 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
